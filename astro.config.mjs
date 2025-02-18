dfaa0d891af76fdbc4ed2c5f9c8bf457fa99a56d
Jan K. Griffioen-Järfalk <jan@jarfalk.se>
2025-02-18 01:38:33 +0100

    Add resolve alias for paths in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import {fileURLToPath, URL} from "url";
+import {defineConfig} from 'astro/config';
 import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  adapter: vercel(),
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    adapter: vercel(),
+    vite: {
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: [
+                {find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url))},
+            ],
+        }
+    }
 });
