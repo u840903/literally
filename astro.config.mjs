a231e77dd18bcca406ac5e7e0cafd842c74c72ca
Jan K. Griffioen-Järfalk <jan@jarfalk.se>
2025-02-18 01:49:38 +0100

    Update Astro and Vite configurations and package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 // @ts-check
-import {fileURLToPath, URL} from "url";
 import {defineConfig} from 'astro/config';
 import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
@@ -7,11 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     adapter: vercel(),
     vite: {
-        plugins: [tailwindcss()],
-        resolve: {
-            alias: [
-                {find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url))},
-            ],
-        }
+        plugins: [tailwindcss()]
     }
 });
